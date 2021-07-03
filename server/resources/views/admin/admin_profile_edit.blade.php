d1ce39b9bb307f6c22742325c6776522caba756c
takaki <cheap_trick_magic@yahoo.co.jp>
2021-07-03 18:08:57 +0900

    Admin Profile & Image Update Part3( jQuery)

--- server/resources/views/admin/admin_profile_edit.blade.php
+++ server/resources/views/admin/admin_profile_edit.blade.php
@@ -1,6 +1,8 @@
 @extends('admin.admin_master')
 
 @section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
 <div class="container-full">
 
     <!-- Main content -->
@@ -43,14 +45,13 @@
                                             <div class="form-group">
                                                 <h5>プロフィール画像 <span class="text-danger">*</span></h5>
                                                 <div class="controls">
-                                                    <input type="file" name="profile_photo_path" class="form-control" required="">
-                                                    <div class="help-block"></div>
+                                                    <input type="file" name="profile_photo_path" class="form-control" required="" id="image">
                                                 </div>
                                             </div>
                                         </div> <!-- end cold md 6 -->
 
                                         <div class="col-md-6">
-                                            <img src="{{ (!empty($editData->profile_photo_path)) ? url('upload/admin_images/' . $editData->profile_photo_path) : url('upload/no_image.jpg') }}" style="width: 100px; height: 100px">
+                                            <img id="showImage" src="{{ (!empty($editData->profile_photo_path)) ? url('upload/admin_images/' . $editData->profile_photo_path) : url('upload/no_image.jpg') }}" style="width: 100px; height: 100px">
                                         </div> <!-- end cold md 6 -->
                                     </div> <!-- end row -->
 
@@ -71,4 +72,15 @@
     </section>
     <!-- /.content -->
 </div>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#image').change(function(e) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#showImage').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+</script>
 @endsection
