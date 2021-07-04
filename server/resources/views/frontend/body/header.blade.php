d36c231b4824c5b0ebe310c0d8580bac9c1234e3
takaki <cheap_trick_magic@yahoo.co.jp>
2021-07-04 18:28:46 +0900

    Frontend Template Register Page Setup

--- server/resources/views/frontend/body/header.blade.php
+++ server/resources/views/frontend/body/header.blade.php
@@ -10,7 +10,11 @@
                         <li><a href="#"><i class="icon fa fa-heart"></i>Wishlist</a></li>
                         <li><a href="#"><i class="icon fa fa-shopping-cart"></i>My Cart</a></li>
                         <li><a href="#"><i class="icon fa fa-check"></i>Checkout</a></li>
-                        <li><a href="#"><i class="icon fa fa-lock"></i>Login</a></li>
+                        @auth
+                        <li><a href="{{ route('login') }}"><i class="icon fa fa-user"></i>ユーザープロフィール</a></li>
+                        @else
+                        <li><a href="{{ route('login') }}"><i class="icon fa fa-lock"></i>ログイン/新規登録</a></li>
+                        @endauth
                     </ul>
                 </div>
                 <!-- /.cnt-account -->
@@ -48,7 +52,7 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-3 logo-holder">
                     <!-- ============================================================= LOGO ============================================================= -->
-                    <div class="logo"> <a href="home.html"> <img src="{{ asset('frontend/assets/images/logo.png') }}" alt="logo"> </a> </div>
+                    <div class="logo"> <a href="{{ url('/') }}"> <img src="{{ asset('frontend/assets/images/logo.png') }}" alt="logo"> </a> </div>
                     <!-- /.logo -->
                     <!-- ============================================================= LOGO : END ============================================================= -->
                 </div>
