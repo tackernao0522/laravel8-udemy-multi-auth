a1af341598eeb6246f7869f03aabc6ec64cfd07d
takaki <cheap_trick_magic@yahoo.co.jp>
2021-07-04 20:10:18 +0900

    User Profile Design Part1

--- server/resources/views/dashboard.blade.php
+++ server/resources/views/dashboard.blade.php
@@ -1,11 +1,29 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Hi..{{ Auth::user()->name }}
-        </h2>
-    </x-slot>
+@extends('frontend.main_master')
 
-    <div class="py-12">
-        This is Home Page
+@section('content')
+<div class="body-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2"><br>
+                <img class="card-img-top" style="border-radius: 50%" src="{{ (!empty($editData->profile_photo_path)) ? Storage::disk('s3')->url("user-profile/{$editData->profile_photo_path}") : url('upload/no_image.jpg') }}" alt="プロフィール画像" height="100%" width="100%"><br><br>
+                <ul class="list-group list-group-flush">
+                    <a href="" class="btn btn-primary btn-sm btn-block">Home</a>
+                    <a href="" class="btn btn-primary btn-sm btn-block">プロフィール更新</a>
+                    <a href="" class="btn btn-primary btn-sm btn-block">パスワード変更</a>
+                    <a href="" class="btn btn-danger btn-sm btn-block">ログアウト</a>
+                </ul>
+            </div> <!-- end col-md-2 -->
+
+            <div class="col-md-2">
+
+            </div> <!-- end col-md-2 -->
+
+            <div class="col-md-6">
+                <div class="card">
+                    <h3 class="text-center"><span class="text-danger">Hi.... </span><strong>{{ Auth::user()->name }}</strong> Welcome to Easy Online Shop</h3>
+                </div>
+            </div> <!-- end col-md-6 -->
+        </div> <!-- end row -->
     </div>
-</x-app-layout>
+</div>
+@endsection
