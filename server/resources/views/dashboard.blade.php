a57cb63b9bf944887b1f9fbe9af4cc9b654459d9
takaki <cheap_trick_magic@yahoo.co.jp>
2021-07-05 12:16:54 +0900

    User Profile: Password Change Part1

--- server/resources/views/dashboard.blade.php
+++ server/resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
                 <ul class="list-group list-group-flush">
                     <a href="" class="btn btn-primary btn-sm btn-block">Home</a>
                     <a href="{{ route('user.profile') }}" class="btn btn-primary btn-sm btn-block">プロフィール更新</a>
-                    <a href="" class="btn btn-primary btn-sm btn-block">パスワード変更</a>
+                    <a href="{{ route('change.password') }}" class="btn btn-primary btn-sm btn-block">パスワード変更</a>
                     <a href="{{ route('user.logout') }}" class="btn btn-danger btn-sm btn-block">ログアウト</a>
                 </ul>
             </div> <!-- end col-md-2 -->
