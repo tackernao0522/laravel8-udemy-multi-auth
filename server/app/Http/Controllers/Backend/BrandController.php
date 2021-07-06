ffbdebe4bf78b6fa85d3bc14fb21d457434dffe1
takaki <cheap_trick_magic@yahoo.co.jp>
2021-07-06 21:38:03 +0900

    Brand Crud Part2

--- server/app/Http/Controllers/Backend/BrandController.php
+++ server/app/Http/Controllers/Backend/BrandController.php
@@ -29,6 +29,7 @@ class BrandController extends Controller
             'brand_image' => 'required|mimes:jpg,jpeg,png',
         ], [
             'brand_name_en.required' => 'Input Brand English Name',
+            'brand_name_en.unique' => 'The brand name ja has already been taken.',
             'brand_image.required' => 'ブランドロゴは必須です。(Input Brand Image.)',
             'brand_image.mimes' => 'ブランドロゴにはjpg, jpeg, pngのうちいずれかの形式のファイルを指定してください。(The brand image must be a file of type: jpg, jpeg, png.)',
         ]);
@@ -57,6 +58,47 @@ class BrandController extends Controller
         );
 
         return redirect()->back()
+            ->with($notification);
+    }
+
+    public function brandEdit($id)
+    {
+        $brand = Brand::findOrFail($id);
+
+        return view('backend.brand.brand_edit', compact('brand'));
+    }
+
+    public function brandUpdate(Request $request, $id)
+    {
+        $brand = Brand::find($id);
+        $validatedData = $request->validate([
+            'brand_name_ja' => 'required',
+            'brand_name_en' => 'required',
+            'brand_image' => 'mimes:jpg,jpeg,png',
+        ], [
+            'brand_name_en.required' => 'Input Brand English Name',
+            'brand_image.mimes' => 'ブランドロゴにはjpg, jpeg, pngのうちいずれかの形式のファイルを指定してください。(The brand image must be a file of type: jpg, jpeg, png.)',
+        ]);
+
+        if ($request->has('brand_image')) {
+            Storage::disk('s3')->delete('/brands/' . $brand->brand_image);
+            $brand->delete();
+            $fileName = $this->saveImage($request->file('brand_image'));
+            $brand->brand_image = $fileName;
+        }
+
+        $brand->brand_name_ja = $request->brand_name_ja;
+        $brand->brand_name_en = $request->brand_name_en;
+        $brand->brand_slug_ja = str_replace(' ', '-', $request->brand_name_ja);
+        $brand->brand_slug_en = strtolower(str_replace(' ', '-', $request->brand_name_en));
+        $brand->save();
+
+        $notification = array(
+            'message' => 'ブランドID：' . $brand->id . 'を更新しました(Brand Updated Successfully)。',
+            'alert-type' => 'info',
+        );
+
+        return redirect()->route('all.brand')
             ->with($notification);
     }
 
