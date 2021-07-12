0c30ce8ef8d1b6956c1cf07ac2c9d3dd2ee8863c
takaki <cheap_trick_magic@yahoo.co.jp>
2021-07-12 16:01:36 +0900

    Fontend Show Category Subcategory and Subsubcategory Part 4

--- server/resources/views/frontend/index.blade.php
+++ server/resources/views/frontend/index.blade.php
@@ -12,24 +12,31 @@
                     <div class="head"><i class="icon fa fa-align-justify fa-fw"></i> @if(session()->get('language') == 'english') Categories @else カテゴリー @endif</div>
                     <nav class="yamm megamenu-horizontal">
                         <ul class="nav">
-
                             @foreach($categories as $category)
                             <li class="dropdown menu-item"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon {{ $category->category_icon }}" aria-hidden="true"></i>@if(session()->get('language') == 'english') {{ $category->category_name_en }} @else {{ $category->category_name_ja }} @endif</a>
                                 <ul class="dropdown-menu mega-menu">
                                     <li class="yamm-content">
                                         <div class="row">
+                                            @php
+                                            $subCategories = App\Models\SubCategory::where('category_id',$category->id)->orderBy('subCategory_name_ja','ASC')->get();
+                                            @endphp
+                                            @foreach($subCategories as $subCategory)
                                             <div class="col-sm-12 col-md-3">
+                                                <h2 class="title">@if(session()->get('language') == 'english') {{ $subCategory->subCategory_name_en }} @else {{ $subCategory->subCategory_name_ja }} @endif</h2>
+
+                                                <!-- Get SubSubCategory Table Data -->
+                                                @php
+                                                $subSubCategories = App\Models\SubSubCategory::where('subCategory_id',$subCategory->id)->orderBy('subSubCategory_name_ja','ASC')->get();
+                                                @endphp
+                                                @foreach($subSubCategories as $subSubCategory)
                                                 <ul class="links list-unstyled">
-                                                    <li><a href="#">Dresses</a></li>
-                                                    <li><a href="#">Shoes </a></li>
-                                                    <li><a href="#">Jackets</a></li>
-                                                    <li><a href="#">Sunglasses</a></li>
-                                                    <li><a href="#">Sport Wear</a></li>
-                                                    <li><a href="#">Blazers</a></li>
-                                                    <li><a href="#">Shirts</a></li>
-                                                    <li><a href="#">Shorts</a></li>
+                                                    <li><a href="#">@if(session()->get('language') == 'english') {{ $subSubCategory->subSubCategory_name_en }} @else {{ $subSubCategory->subSubCategory_name_ja }} @endif</a></li>
                                                 </ul>
+                                                @endforeach
+                                                <!-- end SubSubCategory Foreach -->
                                             </div>
+                                            @endforeach
+                                            <!-- end SubCategory Foreach -->
                                             <!-- /.col -->
                                         </div>
                                         <!-- /.row -->
