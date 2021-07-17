e12f987f6198c05ee787ab01ccbb3f0895579ef9
takaki <cheap_trick_magic@yahoo.co.jp>
2021-07-17 13:14:27 +0900

    Product Tags Show

---
+++ server/resources/views/frontend/common/product_tags.blade.php
@@ -0,0 +1,24 @@
+@php
+$tags_ja = App\Models\Product::groupBy('product_tags_ja')->select('product_tags_ja')->get();
+$tags_en = App\Models\Product::groupBy('product_tags_en')->select('product_tags_en')->get();
+@endphp
+
+<div class="sidebar-widget product-tag wow fadeInUp">
+    <h3 class="section-title">@if(session()->get('language') == 'english') Product tags @else 商品タグ @endif</h3>
+    <div class="sidebar-widget-body outer-top-xs">
+        <div class="tag-list">
+            @if(session()->get('language') == 'english')
+            @foreach($tags_en as $tag)
+            <a class="item active" title="Phone" href="category.html">{{ str_replace(',',' ', $tag->product_tags_en) }}</a>
+            @endforeach
+            @else
+            @foreach($tags_ja as $tag)
+            <a class="item active" title="Phone" href="category.html">{{ str_replace(',',' ', $tag->product_tags_ja) }}</a>
+            @endforeach
+            @endif
+        </div>
+        <!-- /.tag-list -->
+    </div>
+    <!-- /.sidebar-widget-body -->
+</div>
+<!-- /.sidebar-widget -->
