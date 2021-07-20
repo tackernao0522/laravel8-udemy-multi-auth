536a466a40239b537f55207946615a21c62cb6df
takaki <cheap_trick_magic@yahoo.co.jp>
2021-07-20 11:49:10 +0900

    Product View Modal With Ajax Part1

--- server/resources/views/frontend/main_master.blade.php
+++ server/resources/views/frontend/main_master.blade.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta name="description" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="author" content="">
     <meta name="keywords" content="MediaCenter, Template, eCommerce">
     <meta name="robots" content="all">
@@ -154,6 +155,25 @@
     </div>
     <!-- End Add to Cart Product Modal -->
 
+    <script type="text/javascript">
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        })
+        // Start Product View with Modal
+        function productView(id) {
+            // alert(id)
+            $.ajax({
+                type: 'GET',
+                url: '/product/view/modal/' + id,
+                dataType: 'json',
+                success: function(data) {
+                    
+                }
+            })
+        }
+    </script>
 </body>
 
 </html>
