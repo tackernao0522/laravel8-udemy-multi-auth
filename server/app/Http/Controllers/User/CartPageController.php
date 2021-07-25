8c685dd8184ea3b02da4a943e334fe63c27e4fc8
takaki <cheap_trick_magic@yahoo.co.jp>
2021-07-25 15:02:52 +0900

    Coupon Remove & Discount With Ajax Part2

--- server/app/Http/Controllers/User/CartPageController.php
+++ server/app/Http/Controllers/User/CartPageController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 use Gloudemans\Shoppingcart\Facades\Cart;
+use App\Models\Coupon;
 
 class CartPageController extends Controller
 {
@@ -38,6 +40,18 @@ class CartPageController extends Controller
         $row = Cart::get($rowId);
         Cart::update($rowId, $row->qty + 1);
 
+        if (Session::has('coupon')) {
+            $coupon_name = Session::get('coupon')['coupon_name'];
+            $coupon = Coupon::where('coupon_name', $coupon_name)->first();
+
+            Session::put('coupon', [
+                'coupon_name' => $coupon->coupon_name,
+                'coupon_discount' => $coupon->coupon_discount,
+                'discount_amount' => floor((int) Cart::total() * (int) $coupon->coupon_discount / 100),
+                'total_amount' => ceil((int) Cart::total() - (int) Cart::total() * (int) $coupon->coupon_discount / 100),
+            ]);
+        }
+
         return response()->json('increment');
     }
 
@@ -45,6 +59,18 @@ class CartPageController extends Controller
     {
         $row = Cart::get($rowId);
         Cart::update($rowId, $row->qty - 1);
+
+        if (Session::has('coupon')) {
+            $coupon_name = Session::get('coupon')['coupon_name'];
+            $coupon = Coupon::where('coupon_name', $coupon_name)->first();
+
+            Session::put('coupon', [
+                'coupon_name' => $coupon->coupon_name,
+                'coupon_discount' => $coupon->coupon_discount,
+                'discount_amount' => floor((int) Cart::total() * (int) $coupon->coupon_discount / 100),
+                'total_amount' => ceil((int) Cart::total() - (int) Cart::total() * (int) $coupon->coupon_discount / 100),
+            ]);
+        }
 
         return response()->json('decrement');
     }
