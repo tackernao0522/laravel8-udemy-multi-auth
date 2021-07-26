ed2415354a8a224479f31690eb8f7a39837c90ac
takaki <cheap_trick_magic@yahoo.co.jp>
2021-07-26 14:34:06 +0900

    Checkout Page : User Shipping Address Part 2

--- server/app/Http/Controllers/User/CheckoutController.php
+++ server/app/Http/Controllers/User/CheckoutController.php
@@ -28,8 +28,28 @@ class CheckoutController extends Controller
         return json_encode($ships);
     }
 
-    public function checkoutStore(Request $reqeust)
+    public function checkoutStore(Request $request)
     {
-        
+        // dd($reqeust->all());
+        $data = array();
+        $data['shipping_name'] = $request->shipping_name;
+        $data['shipping_email'] = $request->shipping_email;
+        $data['shipping_phone'] = $request->shipping_phone;
+        $data['post_code'] = $request->post_code;
+        $data['division_id'] = $request->division_id;
+        $data['district_id'] = $request->district_id;
+        $data['town_id'] = $request->town_id;
+        $data['notes'] = $request->notes;
+
+        if ($request->payment_method == 'stripe') {
+
+            return view('frontend.payment.stripe', compact('data'));
+        } elseif ($request->payment_method == 'card') {
+
+            return 'card';
+        } else {
+
+            return 'cash';
+        }
     }
 }
