0861ff72c5e639ce54ecbf8af158b3e4ef50bab1
takaki <cheap_trick_magic@yahoo.co.jp>
2021-07-26 12:57:50 +0900

    Checkout Page : Shipping Adress Part 3

--- server/app/Http/Controllers/User/CheckoutController.php
+++ server/app/Http/Controllers/User/CheckoutController.php
@@ -4,8 +4,27 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\ShipDivision;
+use App\Models\ShipDistrict;
+use App\Models\ShipTown;
 
 class CheckoutController extends Controller
 {
-    //
+    public function districtGetAjax($division_id)
+    {
+        $ships = ShipDistrict::where('division_id', $division_id)
+            ->orderBy('district_name', 'DESC')
+            ->get();
+
+        return json_encode($ships);
+    }
+
+    public function townGetAjax($district_id)
+    {
+        $ships = ShipTown::where('district_id', $district_id)
+            ->orderBy('town_name', 'DESC')
+            ->get();
+
+        return json_encode($ships);
+    }
 }
