5159a8a82969bf16b89d0ac4f44d1c09d36104d4
takaki <cheap_trick_magic@yahoo.co.jp>
2021-07-31 15:11:39 +0900

    Admin Panel : Report Page Design

---
+++ server/app/Http/Controllers/Backend/ReportController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ReportController extends Controller
+{
+    public function reportView()
+    {
+        return view('backend.report.report_view');
+    }
+}
