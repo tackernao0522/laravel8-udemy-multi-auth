e170754328b8d866e6d43cc3557f2d2c45823e49
takaki <cheap_trick_magic@yahoo.co.jp>
2021-08-09 16:52:00 +0900

    Products SortBy With Category Option Part1

--- server/app/Http/Controllers/Frontend/ShopController.php
+++ server/app/Http/Controllers/Frontend/ShopController.php
@@ -13,10 +13,24 @@ class ShopController extends Controller
 {
     public function shopPage()
     {
-        $products = Product::where('status', 1)->orderBy('id', 'DESC')->paginate(3);
+        $products = Product::query();
+        if (!empty($_GET['category'])) {
+            $slugs = explode(',', $_GET['category']);
+            $catIds = Category::select('id')
+                ->whereIn('category_slug_ja', $slugs)
+                ->pluck('id')
+                ->toArray();
+            $products = Product::whereIn('category_id', $catIds)->paginate(3);
+        } else {
+            $products = Product::where('status', 1)->orderBy('id', 'DESC')->paginate(3);
+        }
 
         $categories = Category::orderBy('category_name_ja', 'ASC')->get();
 
         return view('frontend.shop.shop_page', compact('products', 'categories'));
+    }
+
+    public function shopFilter(Request $request)
+    {
     }
 }
